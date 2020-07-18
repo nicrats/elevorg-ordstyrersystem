0b9194a5b3b1cc94c41b6ee2bcef5bb99d0078b4
Daniel Martinsen <daniel.martinsen@online.no>
2020-07-19 00:50:19 +0200

    Add layout

--- pages/deltagere.js
+++ pages/deltagere.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import Layout from '../components/layout'
+import styles from '../styles/index.module.scss'
+
 export default function Deltagere() {
-  return <div></div>
+  return <Layout />
 }
