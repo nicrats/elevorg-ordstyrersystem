8f9fabe19bece20a4724b7fd093a1c73fc614447
Daniel Martinsen <daniel.martinsen@online.no>
2020-07-19 00:33:23 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,199 +1,52 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+import styles from '../styles/index.module.scss'
+
 export default function Home() {
   return (
-    <div className='container'>
+    <div className={styles.container}>
       <Head>
         <title>Elevorganisasjonen Taleliste System</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <h1 className='title'>Talerliste</h1>
-        <p className='description'>Talerliste-system laget av Sondre Vinding og Daniel Martinsen</p>
+        <h1 className={styles.title}>Talerliste</h1>
+        <p className={styles.description}>
+          Talerliste-system laget av Sondre Vinding og Daniel Martinsen
+        </p>
 
-        <div className='grid'>
+        <div className={styles.grid}>
           <Link href='/deltagere'>
-            <div className='card'>
+            <div className={styles.card}>
               <h3>Deltagere &rarr;</h3>
               <p>Legg til, fjern og endre deltagere.</p>
             </div>
           </Link>
 
           <Link href='/debattkontroll'>
-            <div className='card'>
+            <div className={styles.card}>
               <h3>Styr ordet &rarr;</h3>
               <p>Styr ordet, og skjerm-grafikken.</p>
             </div>
           </Link>
 
           <Link href='/statistikk'>
-            <div className='card'>
+            <div className={styles.card}>
               <h3>Statistikk &rarr;</h3>
-              <p>Se dagens statistikk basert på debatten.</p>
+              <p>Se dagens statistikk.</p>
             </div>
           </Link>
 
           <Link href='/skjermgrafikk'>
-            <div className='card'>
+            <div className={styles.card}>
               <h3>Fullskjerm &rarr;</h3>
-              <p>Skjerm-grafikk med talere og replikker.</p>
+              <p>Fullskjerms grafikk.</p>
             </div>
           </Link>
         </div>
       </main>
-
-      <footer>
-        <a href='https://elev.no' target='_blank' rel='noopener noreferrer'>
-          Levert for <img src='./logo.png' alt='EO Logo' className='logo' />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-            Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
