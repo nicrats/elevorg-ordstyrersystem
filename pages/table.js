359cfcc608a79829980d1be0ce520fb66b7191d5
sondrevinding <sondrev2023@gmail.com>
2020-07-18 23:16:15 +0200

    Commit 1, Sondre har helt drita kode...

---
+++ pages/table.js
@@ -0,0 +1,238 @@
+import React from "react";
+import { useTable } from "react-table";
+
+const data = [
+  { Number: "1", Name: "Sondre Vinding", Org: "Elevorganisasjonen i Oslo Vest" },
+  { Number: "2", Name: "Daniel Martinsen", Org: "Elevorganisasjonen i Viken" }
+];
+
+const columns = [
+  {
+    Header: "Essential info",
+    columns: [
+      {
+        Header: "Number",
+        accessor: "Number"
+      },
+      {
+        Header: "Name",
+        accessor: "Name"
+      }
+    ]
+  },
+  {
+    Header: "Other Info",
+    columns: [
+      {
+        Header: "Org",
+        accessor: "Org"
+      }
+    ]
+  }
+];
+
+const Table = ({ columns, data }) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+
+export default function App() {
+  return (
+        <div className="App">
+            <main>    
+                <Table columns={columns} data={data} />
+            </main>
+            <footer>
+                <a
+                href="https://elev.no"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Levert for{' '}
+                <img src="./logo.png" alt="EO Logo" className="logo" />
+                </a>
+             </footer>
+                    <style jsx>{`
+                .container {
+                min-height: 100vh;
+                padding: 0 0.5rem;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                }
+
+                main {
+                padding: 5rem 0;
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                }
+
+                footer {
+                width: 100%;
+                height: 100px;
+                border-top: 1px solid #eaeaea;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                }
+
+                footer img {
+                margin-left: 0.5rem;
+                }
+
+                footer a {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                }
+
+                a {
+                color: inherit;
+                text-decoration: none;
+                }
+
+                .title a {
+                color: #0070f3;
+                text-decoration: none;
+                }
+
+                .title a:hover,
+                .title a:focus,
+                .title a:active {
+                text-decoration: underline;
+                }
+
+                .title {
+                margin: 0;
+                line-height: 1.15;
+                font-size: 4rem;
+                }
+
+                .title,
+                .description {
+                text-align: center;
+                }
+
+                .description {
+                line-height: 1.5;
+                font-size: 1.5rem;
+                }
+
+                code {
+                background: #fafafa;
+                border-radius: 5px;
+                padding: 0.75rem;
+                font-size: 1.1rem;
+                font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                    DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+                }
+
+                .grid {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-wrap: wrap;
+
+                max-width: 800px;
+                margin-top: 3rem;
+                }
+
+                .card {
+                margin: 1rem;
+                flex-basis: 45%;
+                padding: 1.5rem;
+                text-align: left;
+                color: inherit;
+                text-decoration: none;
+                border: 1px solid #eaeaea;
+                border-radius: 10px;
+                transition: color 0.15s ease, border-color 0.15s ease;
+                }
+
+                .card:hover,
+                .card:focus,
+                .card:active {
+                color: #0070f3;
+                border-color: #0070f3;
+                }
+
+                .card h3 {
+                margin: 0 0 1rem 0;
+                font-size: 1.5rem;
+                }
+
+                .card p {
+                margin: 0;
+                font-size: 1.25rem;
+                line-height: 1.5;
+                }
+
+                .logo {
+                height: 1em;
+                }
+
+                @media (max-width: 600px) {
+                .grid {
+                    width: 100%;
+                    flex-direction: column;
+                }
+                }
+            `}</style>
+
+            <style jsx global>{`
+                html,
+                body {
+                padding: 0;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                    Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                    sans-serif;
+                }
+
+                * {
+                box-sizing: border-box;
+                }
+            `}</style>
+        </div>
+
+  );
+}
