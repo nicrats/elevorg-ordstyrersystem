d5bb81e94c87bae207dc3568dfaf949e02e736d1
Daniel Martinsen <daniel.martinsen@online.no>
2020-07-19 00:51:28 +0200

    Add menu

--- pages/deltagere.js
+++ pages/deltagere.js
@@ -1,8 +1,6 @@
 import React from 'react'
-
 import Layout from '../components/layout'
-import styles from '../styles/index.module.scss'
 
 export default function Deltagere() {
-  return <Layout />
+  return <Layout></Layout>
 }
