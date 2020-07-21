6d38b87c1871a895c36ae4395f9f1e90e01e73e4
Daniel Martinsen <daniel.martinsen@online.no>
2020-07-21 21:40:50 +0200

    Update graphics design

--- pages/skjermgrafikk.js
+++ pages/skjermgrafikk.js
@@ -7,8 +7,21 @@ export default function Fullskjerm() {
     <div className={styles.main}>
       <div className={styles.beskjedDiv}>Pause frem til 12:00</div>
 
-      <div className={styles.talerDiv}>100 Daniel Martinsen Ås Videregående</div>
-      <div className={styles.talelisteDiv}>400 150 345 651 363 545 485</div>
+      <div className={styles.talelisteDiv}>
+        <p style={{ fontWeight: 700 }}>100</p>
+        <p style={{ fontWeight: 700 }}>Daniel Martinsen</p>
+        <p style={{ fontWeight: 400 }}>Ås Videregående Skole</p>
+      </div>
+
+      <div className={styles.talerDiv}>
+        <p style={{ paddingRight: 35, fontWeight: 700 }}>&rarr; 100</p>
+        <p style={{ paddingRight: 35, fontWeight: 700 }}>Daniel Martinsen</p>
+        <p style={{ paddingRight: 35, fontWeight: 400 }}>Ås Videregående</p>
+      </div>
+
+      <div className={styles.talelisteDiv}>
+        <p>400 150 345 651 363 545 485</p>
+      </div>
     </div>
   )
 }
