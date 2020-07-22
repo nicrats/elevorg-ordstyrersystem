8cf064469104a5d664008f6783a41865400cf67b
Sondre Vinding <sondrevinding@users.noreply.github.com>
2020-07-22 18:38:28 +0200

    Meny oppdatering 1

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { View, Image, StyleSheet } from 'react';
+
+
 
 import styles from '../styles/layout.module.scss'
 
@@ -10,40 +13,38 @@ export default function Layout({ children }) {
         <title>EO Ordstyrersystem</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
       <main>
-        <div className={styles.menugrid}>
-          <Link href='/'>
-            <div className={styles.card}>
-              <h3>Hjem</h3>
-            </div>
-          </Link>
+          <div className={styles.menugrid}>
+            <Link href='/'>
+              <div className={styles.card}>
+                <h3>Hjem</h3>
+              </div>
+            </Link>
 
-          <Link href='/deltagere'>
-            <div className={styles.card}>
-              <h3>Deltagere</h3>
-            </div>
-          </Link>
+            <Link href='/deltagere'>
+              <div className={styles.card}>
+                <h3>Deltagere</h3>
+              </div>
+            </Link>
 
-          <Link href='/debatt'>
-            <div className={styles.card}>
-              <h3>Styr ordet</h3>
-            </div>
-          </Link>
+            <Link href='/debatt'>
+              <div className={styles.card}>
+                <h3>Styr ordet</h3>
+              </div>
+            </Link>
 
-          <Link href='/statistikk'>
-            <div className={styles.card}>
-              <h3>Statistikk</h3>
-            </div>
-          </Link>
+            <Link href='/statistikk'>
+              <div className={styles.card}>
+                <h3>Statistikk</h3>
+              </div>
+            </Link>
 
-          <Link href='/skjermgrafikk'>
-            <div className={styles.card}>
-              <h3>Fullskjerm</h3>
-            </div>
-          </Link>
-        </div>
-
+            <Link href='/skjermgrafikk'>
+              <div className={styles.card}>
+                <h3>Fullskjerm</h3>
+              </div>
+            </Link>
+          </div>
         <div style={{ marginTop: '3rem' }}></div>
 
         {children}
