38817c6693a3fd978b1b63b693e8c095ca652abf
Sondre Vinding <sondrevinding@users.noreply.github.com>
2020-07-22 20:13:27 +0200

    Oppklaring

--- components/layout.js
+++ components/layout.js
@@ -14,37 +14,6 @@ export default function Layout({ children }) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-          <div className={styles.menugrid}>
-            <Link href='/'>
-              <div className={styles.card}>
-                <h3>Hjem</h3>
-              </div>
-            </Link>
-
-            <Link href='/deltagere'>
-              <div className={styles.card}>
-                <h3>Deltagere</h3>
-              </div>
-            </Link>
-
-            <Link href='/debatt'>
-              <div className={styles.card}>
-                <h3>Styr ordet</h3>
-              </div>
-            </Link>
-
-            <Link href='/statistikk'>
-              <div className={styles.card}>
-                <h3>Statistikk</h3>
-              </div>
-            </Link>
-
-            <Link href='/skjermgrafikk'>
-              <div className={styles.card}>
-                <h3>Fullskjerm</h3>
-              </div>
-            </Link>
-          </div>
         <div className={styles.menugrid}>
           <Link href='/'>
             <div className={styles.card}>
